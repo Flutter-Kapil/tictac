2a66a6abaf3fe5be247bfff1903032035ee4989c
kapil jhajhria <jhajhria44@gmail.com>
2019-11-03 08:54:35 +0530

    init commit
    added resources like fonts & backgrounds
    added icon for the app as well
    credit for icon
    https://www.flaticon.com/authors/freepik

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac Toe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TicTacToe(),
+    );
+  }
+}
+
+class TicTacToe extends StatefulWidget {
+  @override
+  _TicTacToeState createState() => _TicTacToeState();
+}
+
+class _TicTacToeState extends State<TicTacToe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
